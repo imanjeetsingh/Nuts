c53a9aff1fd7d8d0a693189c4466742329c55668
singhianjeetsingh <singhianjeetsingh@gmail.com>
2019-11-25 12:52:01 +0530

    changes updated

---
+++ js/carousel.js
@@ -0,0 +1,13 @@
+var flkty = new Flickity( '.main-gallery', {
+    cellAlign: 'center',
+    contain: true,
+    wrapAround: true,
+    prevNextButtons: true,
+    autoPlay: true,
+    autoPlay: 2800,
+    pauseAutoPlayOnHover: true,
+    pageDots: false,
+    
+  });
+
+  
